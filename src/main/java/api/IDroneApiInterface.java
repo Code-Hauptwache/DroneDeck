eca213e5e4f5ed7c976504572965522c86baacd0
Gordon Müller <gordon.mueller@scopewire.de>
2024-12-12 15:02:42 +0100

    Edited IDroneApiInterface Comments

--- src/main/java/api/IDroneApiInterface.java
+++ src/main/java/api/IDroneApiInterface.java
@@ -4,14 +4,9 @@ import main.java.api.dtos.Drone;
 import main.java.api.dtos.DroneDynamics;
 import main.java.api.dtos.DroneType;
 
-import java.net.http.HttpClient;
 import java.util.ArrayList;
 
 public interface IDroneApiInterface {
-    default HttpClient buildHttpClient() {
-        return HttpClient.newBuilder().followRedirects(HttpClient.Redirect.ALWAYS).build();
-    }
-
     /**
      * @return currently used ApiKey
      */
@@ -25,10 +20,10 @@ public interface IDroneApiInterface {
 
     /**
      * Get a list of DroneDynamics
-     * @param limit the number of DroneDynamics to fetch. Must be >= 1
+     * @param limit  the number of DroneDynamics to fetch. Must be between 10_000 and 1
      * @param offset the offset (from 0)
      * @return the list of DroneDynamics
-     * @throws DroneApiException Strange Custom Exception XD
+     * @throws DroneApiException Api Exceptions
      */
     ArrayList<DroneDynamics> getDroneDynamics(int limit, int offset) throws DroneApiException;
 
@@ -36,14 +31,14 @@ public interface IDroneApiInterface {
      * Get 100 DroneDynamics
      * @param offset the offset (from 0)
      * @return 100 DroneDynamics
-     * @throws DroneApiException Magic Exception :D
+     * @throws DroneApiException Api Exceptions
      */
     ArrayList<DroneDynamics> getDroneDynamics(int offset) throws DroneApiException;
 
     /**
      * Get 100 DroneDynamics
      * @return 100 DroneDynamics
-     * @throws DroneApiException Magic Exception :)
+     * @throws DroneApiException Api Exceptions
      */
     ArrayList<DroneDynamics> getDroneDynamics() throws DroneApiException;
 
@@ -51,16 +46,16 @@ public interface IDroneApiInterface {
      * Gets a DroneDynamics by id
      * @param id the id to get the DroneDynamics of
      * @return the drone dynamics
-     * @throws DroneApiException Magic Exception ;)
+     * @throws DroneApiException Api Exceptions
      */
     DroneDynamics getDroneDynamicsById(int id) throws DroneApiException;
 
     /**
      * Get a list of Drones
-     * @param limit the number of Drones to get
+     * @param limit  the number of Drones to get. Must be between 10_000 and 1
      * @param offset the offset (from 0)
      * @return list of Drones
-     * @throws DroneApiException Magic Exception D:
+     * @throws DroneApiException Api Exceptions
      */
     ArrayList<Drone> getDrones(int limit, int offset) throws DroneApiException;
 
@@ -68,14 +63,14 @@ public interface IDroneApiInterface {
      * Get a list of 100 Drones
      * @param offset the offset (from 0)
      * @return a list of 100 Drones
-     * @throws DroneApiException Exception ...
+     * @throws DroneApiException Api Exceptions
      */
     ArrayList<Drone> getDrones(int offset) throws DroneApiException;
 
     /**
      * Get a list of 100 Drones
      * @return a list of 100 Drones
-     * @throws DroneApiException ...
+     * @throws DroneApiException Api Exceptions
      */
     ArrayList<Drone> getDrones() throws DroneApiException;
 
@@ -83,16 +78,16 @@ public interface IDroneApiInterface {
      * Gets a drone by its id
      * @param id the id of the drone to get
      * @return the drone
-     * @throws DroneApiException ...
+     * @throws DroneApiException Api Exceptions
      */
     Drone getDronesById(int id) throws DroneApiException;
 
     /**
      * Get a list of DroneTypes
-     * @param limit the number of Types to get
+     * @param limit  the number of Types to get. Must be between 10_000 and 1
      * @param offset the offset (from 0)
      * @return a list of DroneTypes
-     * @throws DroneApiException ...
+     * @throws DroneApiException Api Exceptions
      */
     ArrayList<DroneType> getDroneTypes(int limit, int offset) throws DroneApiException;
 
@@ -100,14 +95,14 @@ public interface IDroneApiInterface {
      * Get a list of 100 DroneTypes
      * @param offset the offset from 0
      * @return A list with 100 DroneTypes
-     * @throws DroneApiException ...
+     * @throws DroneApiException Api Exceptions
      */
     ArrayList<DroneType> getDroneTypes(int offset) throws DroneApiException;
 
     /**
      * Get a list of 100 DroneTypes
      * @return A list with 100 DroneTypes
-     * @throws DroneApiException ...
+     * @throws DroneApiException Api Exceptions
      */
     ArrayList<DroneType> getDroneTypes() throws DroneApiException;
 
@@ -115,26 +110,26 @@ public interface IDroneApiInterface {
      * Get a DroneType by its id
      * @param id the id of the DroneType to get
      * @return the DroneType
-     * @throws DroneApiException ...
+     * @throws DroneApiException Api Exceptions
      */
     DroneType getDroneTypeById(int id) throws DroneApiException;
 
     /**
      * Gets a drones DroneDynamics
-     * @param id the id of the drone of wich to retrieve the DroneDynamics
-     * @param limit the number of DroneDynamics to get
-     * @param offset the offset
+     * @param id     the id of the drone of which to retrieve the DroneDynamics
+     * @param limit  the number of DroneDynamics to get. Must be between 10_000 and 1
+     * @param offset the offset from 0
      * @return the DroneDynamics of said Drone
-     * @throws DroneApiException ...
+     * @throws DroneApiException Api Exceptions
      */
     ArrayList<DroneDynamics> getDroneDynamicsByDroneId(int id, int limit, int offset) throws DroneApiException;
 
     /**
      * Get 100 DroneDynamics by Drone id
-     * @param id the id of the Drone
-     * @param offset the offset
+     * @param id     the id of the Drone
+     * @param offset the offset from 0
      * @return the DroneDynamics of said Drone
-     * @throws DroneApiException ...
+     * @throws DroneApiException Api Exceptions
      */
     ArrayList<DroneDynamics> getDroneDynamicsByDroneId(int id, int offset) throws DroneApiException;
 
@@ -142,7 +137,7 @@ public interface IDroneApiInterface {
      * Get 100 DroneDynamics by Drone id
      * @param id the id of the Drone
      * @return the DroneDynamics of said Drone
-     * @throws DroneApiException ...
+     * @throws DroneApiException Api Exceptions
      */
     ArrayList<DroneDynamics> getDroneDynamicsByDroneId(int id) throws DroneApiException;
 }
