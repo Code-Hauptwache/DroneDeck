74091e2a0adfc03aaa72f0b721723ad89a2d7cc7
zaka <103366844+zakaBouj@users.noreply.github.com>
2025-01-01 22:55:55 +0100

    Creat InfoTooltip.java

---
+++ src/main/java/ui/components/InfoTooltip.java
@@ -0,0 +1,19 @@
+package main.java.ui.components;
+
+import org.kordamp.ikonli.fontawesome5.FontAwesomeSolid;
+import org.kordamp.ikonli.swing.FontIcon;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class InfoTooltip extends JComponent {
+    private final FontIcon infoIcon;
+
+    public InfoTooltip(String tooltipText) {
+        infoIcon = FontIcon.of(FontAwesomeSolid.INFO_CIRCLE, 16, UIManager.getColor("Label.foreground"));
+        JLabel iconLabel = new JLabel(infoIcon);
+        iconLabel.setToolTipText(tooltipText);
+        setLayout(new BorderLayout());
+        add(iconLabel, BorderLayout.CENTER);
+    }
+}
