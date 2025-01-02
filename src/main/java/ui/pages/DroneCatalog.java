d00e62dc4ee08fe4746f6b2728dd2e0a55a3b61d
zaka <103366844+zakaBouj@users.noreply.github.com>
2025-01-02 13:22:47 +0100

    Bug fix

--- src/main/java/ui/pages/DroneCatalog.java
+++ src/main/java/ui/pages/DroneCatalog.java
@@ -6,6 +6,8 @@ import main.java.ui.dtos.DroneCatalogCardDto;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class DroneCatalog extends JPanel {
     /**
