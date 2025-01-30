6615e1a70fdac2da0630615a07e03b188ef5781d
zaka <103366844+zakaBouj@users.noreply.github.com>
2025-01-30 16:11:52 +0100

    Edit Variable and empty Methode

--- src/main/java/services/AverageSpeed/AverageSpeedService.java
+++ src/main/java/services/AverageSpeed/AverageSpeedService.java
@@ -1,4 +1,11 @@
 package main.java.services.AverageSpeed;
 
 public class AverageSpeedService {
+    private static final int DATA_NUM = 500;
+    private static final int LIMIT = 100;
+    private static final int THREAD_NUM = DATA_NUM / LIMIT;
+
+    public double getAverageSpeed(int droneId) {
+        return 0.0;
+    }
 }
