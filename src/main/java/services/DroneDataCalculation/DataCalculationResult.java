737fcd0dd23aec2c6ca423f58fa4c367591ddafc
Gordon Müller <gordon.mueller@scopewire.de>
2025-01-31 22:17:46 +0100

    Added missing JavaDocs

--- src/main/java/services/DroneDataCalculation/DataCalculationResult.java
+++ src/main/java/services/DroneDataCalculation/DataCalculationResult.java
@@ -1,14 +1,29 @@
 package main.java.services.DroneDataCalculation;
 
+/**
+ * An object that contains the result of the data calculation
+ * like travel distance and average speed
+ */
 public class DataCalculationResult {
 
+    /**
+     * Constructor of the DataCalculationResult
+     * @param travelDistance the total travel distance in kilometers
+     * @param averageSpeed the average speed in kilometers per hour
+     */
     public DataCalculationResult(double travelDistance, double averageSpeed) {
         this.travelDistance = travelDistance;
         this.averageSpeed = averageSpeed;
     }
 
+    /**
+     * The total travel distance in kilometers
+     */
     public double travelDistance;
 
+    /**
+     * The average speed in kilometers per hour
+     */
     public double averageSpeed;
 
 }
