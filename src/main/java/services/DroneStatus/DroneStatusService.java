d8357ddd5a1d4c8bbb0d49d13a094f7862a1685b
Gordon Müller <gordon.mueller@scopewire.de>
2025-02-06 20:35:45 +0100

    Fixed Startup Performance Problems with Threads

--- src/main/java/services/DroneStatus/DroneStatusService.java
+++ src/main/java/services/DroneStatus/DroneStatusService.java
@@ -3,13 +3,23 @@ package main.java.services.DroneStatus;
 import main.java.controllers.DroneController;
 import main.java.entity.DroneEntity;
 import main.java.ui.dtos.DroneDto;
-
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Service to get the status of all drones.
  */
 public class DroneStatusService {
+
+    private static final int THREAD_NUM = 40;
+
+    private final Logger logger = Logger.getLogger(DroneStatusService.class.getName());
+
     private final DroneController droneController;
     private int onlineCount;
     private int issueCount;
@@ -27,15 +37,33 @@ public class DroneStatusService {
 
     private void checkDroneStatuses() {
         List<DroneEntity> drones = droneController.getAllDrones();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_NUM);
+
+        ArrayList<CompletableFuture<String>> futures = new ArrayList<>();
+
         for (DroneEntity drone : drones) {
-            DroneDto droneDto = droneController.getDroneDto(drone);
-            String status = droneDto.getStatus();
-            switch (status) {
-                case "ON" -> onlineCount++;
-                case "IS" -> issueCount++;
-                case "OF" -> offlineCount++;
+            CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+                DroneDto droneDto = droneController.getDroneDto(drone);
+                logger.log(Level.INFO, "Got status for drone " + droneDto.getId());
+                return droneDto.getStatus();
+            }, executorService);
+            futures.add(future);
+        }
+
+        for (CompletableFuture<String> future : futures) {
+            try {
+                String status = future.get();
+                switch (status) {
+                    case "ON" -> onlineCount++;
+                    case "IS" -> issueCount++;
+                    case "OF" -> offlineCount++;
+                }
+            } catch (Exception e) {
+                logger.log(Level.SEVERE, "Failed to get drone status.", e);
             }
         }
+        logger.log(Level.INFO, "Loaded Drone Statuses; Online: " + onlineCount + ", Issue: " + issueCount + ", Offline: " + offlineCount);
     }
 
     public int getOnlineCount() {
